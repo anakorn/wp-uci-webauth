f935ea34f2f1cb60d04afd7cba476a7fb895839c
Alex Nakorn <alexnakorn@gmail.com>
2014-03-09 13:21:28 -0700

    Initial boilerplate commit

---
+++ admin/views/admin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ */
+?>
+
+<div class="wrap">
+
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, nulla, dolorem inventore cumque totam deserunt libero velit quia. Similique, corporis est sit nemo fugit ab obcaecati vero ipsam natus voluptate vel reiciendis. Officia praesentium debitis omnis adipisci ducimus fugiat maiores eligendi facere. Obcaecati beatae consectetur rerum eius esse laudantium quod.</p>
+  <p>Odit, in, a, ipsam, quidem atque fugit voluptatibus incidunt reprehenderit libero autem nisi non tempora quos provident facere accusamus modi impedit voluptates laudantium possimus doloremque dolorem illo ratione similique ex doloribus animi aliquam nihil sapiente aperiam perferendis officia veritatis delectus aliquid asperiores laborum nostrum? Aliquid tempore doloribus sit maiores voluptatum.</p>
+  <p>Quisquam, est modi laborum facere neque quos quibusdam soluta cupiditate ipsum cum tempora pariatur iste quae eveniet inventore fuga atque error hic ad nisi at nulla eum nobis in aut dolor quasi praesentium nihil asperiores sequi fugit molestias quidem excepturi. Animi, eum voluptatibus expedita molestiae nulla sequi earum dolorem saepe?</p>
+  <p>Aliquam, porro, neque, quae, laboriosam fugit optio distinctio molestiae animi consequatur incidunt aut dolorum doloribus velit beatae voluptate veritatis veniam nihil molestias saepe itaque tempora reprehenderit expedita sint consequuntur hic repellat totam id fuga maiores at a provident vel iure officia mollitia nisi pariatur. Suscipit cum neque est inventore deleniti.</p>
+
+</div>
